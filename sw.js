cfd11bd26ebd77f1be1832f2d2249de77f5242c9
itjshp <itjshp@gmail.com>
2026-07-01 16:47:48 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mini-stock-credit-v7";
+const CACHE_NAME = "khaikhong-v8";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icons/icon-192.svg", "./icons/icon-512.svg"];
 
 self.addEventListener("install", (event) => {
